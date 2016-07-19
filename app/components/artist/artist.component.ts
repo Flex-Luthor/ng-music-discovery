0214fe0da8a517d3a62f90a2ea6998b9a6b3d5ad
alohaglenn <glenngonda@gmail.com>
2016-07-19 00:06:49 -0700

    add spotify API method for artist + render artist route

--- app/components/artist/artist.component.ts
+++ app/components/artist/artist.component.ts
@@ -7,15 +7,31 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   moduleId: module.id,
   selector: 'artist',
-  templateUrl: 'artist.component.html'
+  templateUrl: 'artist.component.html',
+  styleUrls: ['artist.component.css']
 })
 
 export class ArtistComponent {
-  id: any;
+  id: string;
+  newId: string;
   artist: Artist[];
   albums: Album[];
 
-  constructor() {
+  constructor(private _spotifyService: SpotifyService, private _router: Router, private _route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.id = this._route.params
+      .subscribe(params => {
+        this.newId = params['id'];
+      })
+
+    this._spotifyService.getArtist(this.newId)
+      .subscribe(artist => {
+        console.log('artist: ', artist);
+        this.artist = artist;
+      })
 
   }
 
--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/map';
 export class SpotifyService {
 
   private searchUrl: string;
+  private artistUrl: string;
 
   constructor(private _http: Http) {
 
@@ -15,6 +16,12 @@ export class SpotifyService {
   searchArtist(sArtist:string) {
     this.searchUrl = 'https://api.spotify.com/v1/search?query=' + sArtist + '&limit=8&type=artist';
     return this._http.get(this.searchUrl)
+      .map(res => res.json());
+  }
+
+  getArtist(id:string) {
+    this.artistUrl = 'https://api.spotify.com/v1/artists/' + id;
+    return this._http.get(this.artistUrl)
       .map(res => res.json());
   }
 
